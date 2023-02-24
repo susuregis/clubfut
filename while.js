0c1877ae71f750bcd2bf4341aa4a93703fab0bd7
susuregis <125358593+susuregis@users.noreply.github.com>
2023-02-24 12:53:18 -0300

    Add files via upload

---
+++ while.js
@@ -0,0 +1,17 @@
+var numero;
+var contador = 0;
+numero=101;
+if ((numero<10) || (numero>100)){
+
+
+
+}else {
+    while(contador<=numero)
+    {
+        console.log(contador);
+        contador++
+
+    }
+
+
+}
