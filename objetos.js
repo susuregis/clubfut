730f0ee19653b98bc1862201636fcd469df5be9d
susuregis <125358593+susuregis@users.noreply.github.com>
2023-03-09 11:41:32 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,44 @@
+/* var carro= {
+    modelo: "freemond",
+    ano : 2014,
+    cor : "branco",
+    quilometragem:50000, 
+
+}; 
+console.log(carro); */
+
+/* var pessoas = {
+    nome: "suelen regina",
+    idade: 17,
+    email:"suelenrfalves@cetii.pe.senac.br",
+    profissão: "tecnico em informatica",
+    endereço: "rua marques de baependi 572 casa 2",
+    filiação:["sony regina","roberto nivaldo"]
+
+}
+console.log(pessoas.nome+"," + pessoas.idade+ ","+ pessoas.email+","+ pessoas.profissão+","+ pessoas.endereço+","+pessoas.filiação);
+console.log("------------------");
+console.log(pessoas.nome+","+ pessoas.profissão);
+console.log("-----------")
+console.log(pessoas.nome+","+pessoas.filiação[0]); */
+
+
+var casadecamp={
+    casadecamp: 'casa de campo',
+    local:'recife',
+    cor :'amarelo',
+    metros:'200',
+    numero:'260',
+         casadepraia:{
+            casadepraia:'casa de praia',
+            lugar:'são paulo',
+            cor:'preto',
+            metros:' 300',
+            numero:'120',
+
+
+    }
+}
+console.log(casadecamp.casadecamp+","+casadecamp.local+","+casadecamp.cor+","+casadecamp.metros+","+casadecamp.numero);
+console.log("---------------------");
+console.log(casadepria.casadepraia,casadepraia+":"+casadecamp.casadepraia.lugar+","
