6ed65a663261a718f468b8c1cbe5610cc84aa9de
susuregis <125358593+susuregis@users.noreply.github.com>
2023-03-09 18:33:19 -0300

    Add files via upload

---
+++ att4.js
@@ -0,0 +1,36 @@
+
+
+
+
+/* var pessoas = {
+    nome: "suelen regina",
+    idade: 17,
+    email:"suelenrfalves@cetii.pe.senac.br",
+    profissão: "tecnico em informatica",
+    endereço: "rua marques de baependi 572 casa 2",
+    filiação:["sony regina","roberto nivaldo"]
+}
+console.log(pessoas.nome+"," + pessoas.idade+ ","+ pessoas.email+","+ pessoas.profissão+","+ pessoas.endereço+","+pessoas.filiação);
+console.log("------------------");
+console.log(pessoas.nome+","+ pessoas.profissão);
+console.log("-----------")
+console.log(pessoas.nome+","+pessoas.filiação[0]); */
+
+
+
+
+
+
+
+
+
+
+var animais={
+    nome:"cachorro",
+    idade:1,
+    vacinação:"todas concluidas",
+    raça:"golden",
+    caracteristicas:"Companheiro, corajoso e amoroso"
+
+}
+console.log(animais.nome+","+ animais.idade+","+ animais.vacinação+","+ animais.raça+","+ animais.caracteristicas);
