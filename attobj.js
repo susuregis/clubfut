007d687013fe7edb385ed47ebf9014dabe657369
susuregis <125358593+susuregis@users.noreply.github.com>
2023-03-09 22:47:38 -0300

    Add files via upload

---
+++ attobj.js
@@ -0,0 +1,33 @@
+//Crie um objeto armazém com propriedades localização, ano de fundação, responsável, array de produtos, horário de funcionamento e array dias de funcionamento,
+
+//Imprima as propriedades do objeto. •Imprima a localização, horário e os dias de funcionamento.
+
+
+/* var armazém ={
+    localização : "campo grande",
+    anodefundaçao: 1989,
+    responsavel:"joel",
+    dias:["segunda das 7:00 até as 18:00,terça das 7:00 até as 18:00,quarta das 7:00 até as 18:00,quinta das 7:00 até as 18:00,sexta das 7:00 até as 18:00 e sabados e domingos são fechados"],
+    produtos:['ceramicas','tijolo','portas','janelas','arreia','pedras',],
+
+}
+console.log(armazém.localização+","+ armazém.anodefundaçao+","+ armazém.responsavel)
+console.log("-----------------------")
+console.log(armazém.dias)
+console.log (armazém.produtos) */
+
+
+//Crie um objeto de filmes com as propriedades título, diretor, atores principais, array de gênero, ano de lançamento.
+
+//Imprima as propriedades do objeto.
+
+//Imprima as propriedades de seu filme preferido.
+
+var filmes ={
+    título:"ALADDIN",
+    diretor:"Guy Ritchie",
+    atoresprin:['Scott Weinger, Jonathan Freeman, Robin Williams, Linda Larkin, Frank Welker, Gilbert Gottfried , Douglas Seale,'],
+    gênero:['romance','aventura','ficcição'],
+    anolançamento:1992,
+}
+console.log(filmes.título+","+ filmes.diretor+','+ filmes.atoresprin+","+ filmes.gênero+","+ filmes.anolançamento)
