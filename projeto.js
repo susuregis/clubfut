16186c7b283f309bc4957b5c5452e5964b48ae9d
susuregis <125358593+susuregis@users.noreply.github.com>
2023-03-21 12:28:52 -0300

    Add files via upload

---
+++ projeto.js
@@ -0,0 +1,45 @@
+var imc= 17;
+var massa =55;
+var altura=1.60;
+var calculo=(altura*altura/massa)
+
+
+
+if (calculo>=18,5 && calculo<=24,9){
+    console.log("SEU PESO ESTÁ NORMAL");
+
+}else if(calculo)
+
+
+
+/* var alimentação='perda de gordura e ganho de massa'
+
+switch (alimentação) {
+    case "ganho de massa":
+        console.log("Alimentos ricos em proteínas são muito importantes para ganhar massa magra, então a sua dieta vai ser:")
+        console.log("Café da manhã: ovos mexidos com orégano torrada integral e suco de abacaxi");
+        console.log("Lanche da manhã:Mousse de abacate com cacau");
+        console.log("Almoço :espaguete à bolonhesa");
+        console.log("Lanche da tarde: iogurte Integral granola");
+        console.log("Jantar: empadão de frango e salada verde");
+        break
+    case "perda de peso":
+        console.log("Além da dieta é necessarioBeber bastante água,Mastigar bem e devagar os alimentos,Dormir de 7 a 9 horas por noite." )
+        console.log("Café da manhã:200 ml de leite desnatado + omelete feita com 1 ovo e tomate picado");
+        console.log("Lanche da manhã:1 pera")
+        console.log("Almoço: 1 posta de peixe ensopado + 1 prato de salada com 2 col de sopa de grão de bico, tomate, pepino, cebola e alface, temperada com 1 col sobremesa de azeite + 2 fatias de abacaxi");
+        console.log("Lanche da tarde:1 iogurte desnatado s/ açúcar + 1 col de sopa de sementes de abóbora");
+        console.log("Jantar:1 omelete com 2 ovos, 1 fio de azeite, tomate, orégano e espinafre +   + 6 morangos");
+        console.log("Para emagrecer de forma saudável, é importante priorizar os alimentos com pouca gordura e calorias.");
+         break
+    case "perda de gordura e ganho de massa":
+        console.log("Café da manhã:1 xícara de leite desnatado ou de bebida vegetal + 1 panqueca de aveia com pasta de amendoim + 1 fruta");
+        console.log("Lanche da manha:1 torrada integral com queijo + 1 fruta");
+        console.log("Almoço/ Jantar:150 gramas de carne + 4 colheres de arroz integral + 2 colheres de sopa de feijão + salada de alface, tomate e beterraba + 1 colher de sopa de óleo de coco ou Macarrão integral com atum e molho de tomate natural + salada de repolho refogado, cenoura ralada e milho + 1 colher de chá de linhaça + 1 colher de sopa de azeite de oliva + 1 fruta");
+        console.log("Lanche da tarde :1 iogurte natural desnatado + sanduíche de frango com requeijão light");
+         break
+    default:
+        console.log("não existe esse plano alimentar")
+
+    
+}    */
