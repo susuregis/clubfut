c830df040c2236046654d6d48db85cb6c9165e15
susuregis <125358593+susuregis@users.noreply.github.com>
2023-03-28 12:41:51 -0300

    Add files via upload

---
+++ imobiliaria.js
@@ -0,0 +1,48 @@
+/* class Clientes {
+    constructor(nome, idade, cpf, endereco, email){
+      this.nome = nome
+      this.idade = idade
+      this.cpf = cpf
+      this.endereco = endereco
+      this.email = email
+    }
+  }
+  
+  const cadastros = [
+    new Clientes('Suelen', 20, '73533378967', 'Rua manuel Bandeira', 'suelen@gmail.com'),
+    new Clientes('Pedro', 40, '34523456489', 'Rua Costa franca', 'pedro@gmail.com'),
+    new Clientes('Carlos', 34, '8875548601', 'Rua augusto de freitas', 'carlos@gmail.com'),
+    new Clientes('Robson', 33, '99744422234', 'Rua feitosa de sá', 'robson@gmail.com'),
+    new Clientes('Rodrigo', 55, '23433323445', 'Rua Salvador de Sá', 'rodrigo@gmail.com'),
+  ]
+  
+  for (let i = 0; i < cadastros.length; i++) {
+    console.log(`${cadastros[i].nome} ${cadastros[i].idade} ${cadastros[i].cpf} ${cadastros[i].email}`);
+  } */
+  
+
+
+
+
+
+
+
+
+
+  /* class Car {
+    constructor(make, model, year) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+    }
+  }
+
+  const cars = [
+    new Car('Honda', 'Civic', 2021),
+    new Car('Toyota', 'Corolla', 2020),
+    new Car('Ford', 'Mustang', 2019)
+  ];
+
+  for (let i = 0; i < cars.length; i++) {
+    console.log(`${cars[i].make} ${cars[i].model} (${cars[i].year})`);
+  } */
